e949c95ed28d12ef5514ffb5fcb8b78add2243d9
IvanIvanReg27 <makhorin0088@gmail.com>
2023-09-22 01:31:59 +1000

    servlet ready

--- src/main/java/servlets/MainServlet.java
+++ src/main/java/servlets/MainServlet.java
@@ -11,13 +11,22 @@ public class MainServlet extends HttpServlet {
     @Override
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
+        log("Method init test...");
     }
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("Method service enter test...\n");
         super.service(req, resp);
+        resp.getWriter().write("Method service exit test...\n");
     }
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("Method doGet test...\n");
+    }
+
     @Override
     public void destroy() {
         super.destroy();
+        log("Method destroy test...");
     }
 }
