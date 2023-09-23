8a97b9693f5576d59f9a3c287da97c7fbf4a9da1
IvanIvanReg27 <makhorin0088@gmail.com>
2023-09-24 02:28:02 +1000

    jdbc driver ready

--- src/main/java/servlets/EmployeesServlet.java
+++ src/main/java/servlets/EmployeesServlet.java
@@ -1,6 +1,5 @@
 package servlets;
 
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,23 +10,16 @@ import java.sql.*;
 
 public class EmployeesServlet extends HttpServlet {
     @Override
-    public void init(ServletConfig config) throws ServletException {
-        super.init(config);
-    }
-    @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.service(req, resp);
-    }
-    @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         PrintWriter pw = resp.getWriter();
         try {
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
         try {
-            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3308/hr", "root", "Jrheu666666");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3308/hr",
+                    "root", "xxx");
             Statement stmt = conn.createStatement();
             ResultSet rs = stmt.executeQuery("SELECT first_name FROM employees");
 
@@ -42,8 +34,5 @@ public class EmployeesServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doPost(req, resp);
-    }
-    @Override
-    public void destroy() {
     }
 }
--- src/main/java/test/TestClass.java
+++ src/main/java/test/TestClass.java
@@ -2,6 +2,6 @@ package test;
 
 public class TestClass {
     public String getInfo() {
-        return "Эта строка вернулась из тестового класса";
+        return "Checking jsp servlet";
     }
 }
